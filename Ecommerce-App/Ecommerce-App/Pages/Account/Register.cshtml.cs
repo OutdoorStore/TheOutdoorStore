27d37ede1a770d6d4a85831fe1f0fd4860f2e444
Peyton-Cysewski <github.peyton.cysewski@gmail.com>
2020-08-11 16:16:30 -0700

    Added form and used a layout to incorporate bootstrap

---
+++ Ecommerce-App/Ecommerce-App/Pages/Account/Register.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ecommerce_App.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ecommerce_App.Pages.Account
+{
+    public class RegisterModel : PageModel
+    {
+        private UserManager<Customer> _userManager;
+        public RegisterModel(UserManager<Customer> userManager)
+        {
+            _userManager = userManager;
+        }
+        public RegisterViewModel Input { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+
+        }
+
+        public class RegisterViewModel
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+            public string ConfirmPassword { get; set; }
+        }
+    }
+}
