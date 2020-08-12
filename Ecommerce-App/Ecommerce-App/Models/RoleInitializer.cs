59c9d76126b57c143ba1df24d3e2a307cfb30990
Peyton-Cysewski <github.peyton.cysewski@gmail.com>
2020-08-12 16:14:59 -0700

    periodic update

---
+++ Ecommerce-App/Ecommerce-App/Models/RoleInitializer.cs
@@ -0,0 +1,65 @@
+﻿using Ecommerce_App.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce_App.Models
+{
+    public class RoleInitializer
+    {
+        private static readonly List<IdentityRole> Roles = new List<IdentityRole>()
+        {
+            new IdentityRole
+            {
+                Name = ApplicationRoles.Admin,
+                NormalizedName = ApplicationRoles.Admin.ToUpper(),
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            }
+        };
+
+        public static void SeedData(IServiceProvider serviceProvider, UserManager<Customer> users, IConfiguration _config)
+        {
+            using (var dbContext = new UserDbContext(serviceProvider.GetRequiredService<DbContextOptions<UserDbContext>>()))
+            {
+                dbContext.Database.EnsureCreated();
+                AddRoles(dbContext);
+                SeedUsers(users, _config);
+            }
+        }
+
+        private static void SeedUsers(UserManager<Customer> userManager, IConfiguration _config)
+        {
+            if (userManager.FindByEmailAsync(_config["AdminEmail"]).Result == null)
+            {
+                Customer user = new Customer();
+                user.UserName = _config["AdminEmail"];
+                user.Email = _config["AdminEmail"];
+                user.FirstName = "Internet";
+                user.LastName = "Overlord";
+
+                IdentityResult result = userManager.CreateAsync(user, _config["AdminPassword"]).Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, ApplicationRoles.Admin).Wait();
+                }
+            }
+        }
+
+        private static void AddRoles(UserDbContext context)
+        {
+            if (context.Roles.Any()) return;
+
+            foreach(var role in Roles)
+            {
+                context.Roles.Add(role);
+                context.SaveChanges();
+            }
+        }
+    }
+}
