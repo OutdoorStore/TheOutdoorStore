7a635ce48bd87c4dc540f7a2baf7ad9bea5b73ae
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-08-12 14:05:10 -0700

    Completed Logout.cshtml.cs set up

--- Ecommerce-App/Ecommerce-App/Pages/Account/Logout.cshtml.cs
+++ Ecommerce-App/Ecommerce-App/Pages/Account/Logout.cshtml.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ecommerce_App.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,9 +11,21 @@ namespace Ecommerce_App.Pages.Account
 {
     public class LogoutModel : PageModel
     {
+        private SignInManager<Customer> _signInManager;
+
+        public LogoutModel(SignInManager<Customer> signIn)
+        {
+            _signInManager = signIn;
+        }
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
