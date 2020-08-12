2754a92013c185e10dc73acd3bdaf9c979399f59
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-08-12 14:39:14 -0700

    Created the Blob service

---
+++ Ecommerce-App/Ecommerce-App/Models/Services/Blob.cs
@@ -0,0 +1,18 @@
+﻿using Ecommerce_App.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce_App.Models.Services
+{
+    public class Blob : IImage
+    {
+        // All of our logic to manage the uploading of blob. 
+
+        public Task UploadImage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
