7a608c337e08c3e1e8a4c351a7d2495c32e60de2
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-08-23 12:09:43 -0700

    Updated summary comments in the Cart service and interface.

--- Ecommerce-App/Ecommerce-App/Models/Interfaces/ICart.cs
+++ Ecommerce-App/Ecommerce-App/Models/Interfaces/ICart.cs
@@ -7,19 +7,38 @@ namespace Ecommerce_App.Models.Interfaces
 {
     public interface ICart
     {
-        //Create
+        /// <summary>
+        /// Creates a new entry in the Cart database table
+        /// based on the userId parameter, and returns the new cart object
+        /// </summary>
+        /// <param name="userId">The unique id of the user that is creating the cart</param>
+        /// <returns>The newly created Cart object</returns>
         Task<Cart> Create(string userId);
 
-        // Get All
+        /// <summary>
+        /// Calculates the total price of all the cart items within the cart, 
+        /// based on the user Id, searches for an active cart, lists all of the cart items, 
+        /// calculates the total of each cart item, and returns a total of totals. 
+        /// </summary>
+        /// <param name="userId">The unique Id of the signed in user</param>
+        /// <returns>The total price of all cart items in cart</returns>
         decimal GetCartTotal(string userId);
 
-        // Get Single Cart
+        /// <summary>
+        /// Gets the user's active cart from the database, if exists, 
+        /// based on the signed in user Id. 
+        /// </summary>
+        /// <param name="userId">The unique Id of the signed in user</param>
+        /// <returns>The user's active cart object</returns>
         Task<Cart> GetActiveCartForUser(string userId);
 
-        //Update
+        /// <summary>
+        /// Updates a cart's Active status from true to false, 
+        /// which effectively "closes" it for any other updates.
+        /// </summary>
+        /// <param name="userId">The unique Id of the signed in user</param>
+        /// <returns>The updated closed cart object(active = false)</returns>
         Task<Cart> CloseCart(string userId);
 
-        // Delete
-        Task Delete(int id);
     }
 }
--- Ecommerce-App/Ecommerce-App/Models/Services/CartService.cs
+++ Ecommerce-App/Ecommerce-App/Models/Services/CartService.cs
@@ -21,6 +21,12 @@ namespace Ecommerce_App.Models.Services
             _cartItem = cartItem;
         }
 
+        /// <summary>
+        /// Creates a new entry in the Cart database table
+        /// based on the userId parameter, and returns the new cart object
+        /// </summary>
+        /// <param name="userId">The unique id of the user that is creating the cart</param>
+        /// <returns>The newly created Cart object</returns>
         public async Task<Cart> Create(string userId)
         {
             Cart cart = new Cart()
@@ -34,23 +40,12 @@ namespace Ecommerce_App.Models.Services
             return cart;
         }
 
-        // TODO: update this method to include all of the cart items and products
-        // then inject it into the cartcomponent view so there's no DB in the view
-        //public List<Cart> GetCartsForUser(string userId)
-        //{
-        //    List<Cart> result = _storeContext.Carts.Where(c => c.UserId == userId)
-        //                                           .ToList();
-
-
-
-        //    return result;
-        //}
-
-        public Task Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
-
+        /// <summary>
+        /// Gets the user's active cart from the database, if exists, 
+        /// based on the signed in user Id. 
+        /// </summary>
+        /// <param name="userId">The unique Id of the signed in user</param>
+        /// <returns>The user's active cart object</returns>
         public async Task<Cart> GetActiveCartForUser(string userId)
         {
             var customerCart = await _storeContext.Carts.Where(c => c.UserId == userId && c.Active == true)
@@ -61,6 +56,13 @@ namespace Ecommerce_App.Models.Services
             return customerCart;
         }
 
+        /// <summary>
+        /// Calculates the total price of all the cart items within the cart, 
+        /// based on the user Id, searches for an active cart, lists all of the cart items, 
+        /// calculates the total of each cart item, and returns a total of totals. 
+        /// </summary>
+        /// <param name="userId">The unique Id of the signed in user</param>
+        /// <returns>The total price of all cart items in cart</returns>
         public decimal GetCartTotal(string userId)
         {
             decimal total = 0;
@@ -73,6 +75,12 @@ namespace Ecommerce_App.Models.Services
             return total;
         }
 
+        /// <summary>
+        /// Updates a cart's Active status from true to false, 
+        /// which effectively "closes" it for any other updates.
+        /// </summary>
+        /// <param name="userId">The unique Id of the signed in user</param>
+        /// <returns>The updated closed cart object(active = false)</returns>
         public async Task<Cart> CloseCart(string userId)
         {
             Cart cart = await GetActiveCartForUser(userId);
