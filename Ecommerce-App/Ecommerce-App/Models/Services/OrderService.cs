cff67492d601459389f68ded78b51c59de1f467d
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-08-23 12:35:16 -0700

    Added order interface and service summary notes.

--- Ecommerce-App/Ecommerce-App/Models/Services/OrderService.cs
+++ Ecommerce-App/Ecommerce-App/Models/Services/OrderService.cs
@@ -16,26 +16,18 @@ namespace Ecommerce_App.Models.Services
         {
             _storeContext = storeContext;
         }
+
+        /// <summary>
+        /// Creates a new entry in the Order database table
+        /// with all of the details of the finalized order
+        /// </summary>
+        /// <param name="order">The order object to be finalized</param>
+        /// <returns>The created order object</returns>
         public async Task<Order> FinalizeOrder(Order order)
         {
             _storeContext.Entry(order).State = EntityState.Added;
             await _storeContext.SaveChangesAsync();
             return order;
-        }
-
-        public Task<List<Order>> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Order> GetOrder(string userId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Order> GetOrder(int id)
-        {
-            throw new NotImplementedException();
         }
     }
 }
