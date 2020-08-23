5d22e6bdb448b99f19f163b3e9068f7c77a87b6e
Naama Bar-Ilan <barilan.naama@gmail.com>
2020-08-23 12:51:54 -0700

    Updated summary comments in products service and interface.

--- Ecommerce-App/Ecommerce-App/Models/Services/ProductsService.cs
+++ Ecommerce-App/Ecommerce-App/Models/Services/ProductsService.cs
@@ -18,6 +18,14 @@ namespace Ecommerce_App.Models.Services
         {
             _storeDbContext = storeDbContext;
         }
+
+        /// <summary>
+        /// Creates a new entry in the database table
+        /// based on the product object parameter
+        /// and returns the same object. 
+        /// </summary>
+        /// <param name="product">An instance of Product</param>
+        /// <returns>A specific Product object</returns>
         public async Task<Product> CreateProduct(Product product)
         {
             _storeDbContext.Entry(product).State = EntityState.Added;
@@ -26,6 +34,12 @@ namespace Ecommerce_App.Models.Services
             return product;
         }
 
+        /// <summary>
+        /// Deletes a product from the products database table
+        /// based on the product Id parameter.
+        /// </summary>
+        /// <param name="id">A unique integer product Id value</param>
+        /// <returns>An empty task object</returns>
         public async Task DeleteProduct(int id)
         {
             Product product = await _storeDbContext.Products.FindAsync(id);
@@ -41,6 +55,12 @@ namespace Ecommerce_App.Models.Services
             }
         }
 
+        /// <summary>
+        /// Returns a specific product from the database table,
+        /// by product Id.
+        /// </summary>
+        /// <param name="id">A unique integer product Id value</param>
+        /// <returns>A specific product</returns>
         public async Task<Product> GetSingleProduct(int id)
         {
             Product product = await _storeDbContext.Products.FindAsync(id);
@@ -55,6 +75,13 @@ namespace Ecommerce_App.Models.Services
             }
         }
 
+        /// <summary>
+        /// Updates a specific product in the database table, 
+        /// by product Id, into the new product object passed as parameter.
+        /// </summary>
+        /// <param name="id">A unique integer product Id value</param>
+        /// <param name="product">An instance of Product</param>
+        /// <returns>The updated Product object</returns>
         public async Task<Product> UpdateProduct(int id, Product product)
         {
             if(product == null)
@@ -71,6 +98,12 @@ namespace Ecommerce_App.Models.Services
             }
         }
 
+
+        /// <summary>
+        /// Returns a collection of all the products in the 
+        /// database.
+        /// </summary>
+        /// <returns>A collection of all products</returns>
         public async Task<List<Product>> GetAllProducts()
         {
             var products = await _storeDbContext.Products.ToListAsync();
