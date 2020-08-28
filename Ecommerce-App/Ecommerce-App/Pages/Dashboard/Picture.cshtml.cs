15c14693922fd93af0ab90ecb1358df1eb8b0dae
Peyton-Cysewski <github.peyton.cysewski@gmail.com>
2020-08-28 10:20:17 -0700

    added create and delete to the page

--- Ecommerce-App/Ecommerce-App/Pages/Dashboard/Picture.cshtml.cs
+++ Ecommerce-App/Ecommerce-App/Pages/Dashboard/Picture.cshtml.cs
@@ -21,9 +21,6 @@ namespace Ecommerce_App.Pages.Dashboard
     [Authorize(Policy = "AdminOnly")]
     public class PictureModel : PageModel
     {
-        public List<Order> Orders { get; set; }
-        public Cart Cart { get; set; }
-
         private SignInManager<Customer> _signInManager;
         private UserManager<Customer> _userManager;
         private IOrder _order;
@@ -41,24 +38,56 @@ namespace Ecommerce_App.Pages.Dashboard
             _order = order;
         }
 
+        public List<Order> Orders { get; set; }
+        public List<Product> Products { get; set; }
+
         [BindProperty]
         public ImageViewModel Input { get; set; }
       
         [BindProperty]
         public Product Product { get; set; }
 
-        public async Task<IActionResult> OnGet(int id)
+        public async Task<IActionResult> OnGet()
         {
             // use _product service to get the product by id.
             // set the product property to our service result:
-            Product = await _productService.GetSingleProduct(id);
+            Products = await _productService.GetAllProducts();
 
             Orders = await _order.GetAllOrdersForAdmin();
 
             return Page();
         }
 
-        public async Task<IActionResult> OnPost()
+        public async Task<IActionResult> OnPostCreate()
+        {
+            string ext = Path.GetExtension(Input.File.FileName);
+            string fileName = Input.File.FileName;
+            string contentType = Input.File.ContentType;
+
+            if (Input.File != null)
+            {
+                using (var stream = new MemoryStream())
+                {
+                    // copies the file as a stream to the file location:
+                    await Input.File.CopyToAsync(stream);
+                    var bytes = stream.ToArray();
+                    await _image.UploadImage("images", fileName, bytes, contentType);
+                }
+
+            }
+
+            Blob blob = new Blob(_configuration);
+
+            var resultBlob = await blob.GetBlob(Input.File.FileName, "images");
+
+            string imageUri = resultBlob.Uri.ToString();
+            Product.Image = imageUri;
+            Product.Id = 0;
+            await _productService.CreateProduct(Product);
+            return RedirectToPage("/Dashboard/Picture");
+        }
+
+        public async Task<IActionResult> OnPostUpdate()
         {
             // Create a local file in the ./data/ directory for uploading and downloading
             // Set the local path to a temp location (folder)
@@ -95,6 +124,13 @@ namespace Ecommerce_App.Pages.Dashboard
             return RedirectToAction("Index", "Home");
         }
 
+        public async Task<IActionResult> OnPostDelete()
+        {
+            await _productService.DeleteProduct(Product.Id);
+            return RedirectToPage("/Dashboard/Picture");
+        }
+
+        // refactor, view model not needed on a razor page.
         public class ImageViewModel
         {
             public IFormFile File { get; set; }
